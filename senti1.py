d8715ae60c4e1f146b35032c25d5fd496097edd6
Siddharth Mittal <smittal6@gmail.com>
2017-01-17 00:40:20 +0530

    Add export code

--- senti1.py
+++ senti1.py
@@ -1,7 +1,7 @@
 import pyprind
 import pandas as pd
 import os
-
+import numpy as np
 basepath='aclImdb'
 
 labels={'pos':1,'neg':0}
@@ -15,6 +15,11 @@ for s in ('test','train'):
             with open(os.path.join(path,file),'r',encoding='utf-8') as infile:
                 # read only mode
                 txt=infile.read()
-            df=df.append([txt,labels[l]],ignore_index=True)
+            df=df.append([[txt,labels[l]]],ignore_index=True)
             pbar.update()
-# df.columns=['review','sentiment']
+df.columns=['review','sentiment']
+print(df)
+#Now adding the code for exporting to csv. We don't want to spend 10 mins loading it every time.
+np.random.seed(0)
+df=df.reindex(np.random.permutation(df.index))
+df.to_csv('./movie_data.csv',index=False)
