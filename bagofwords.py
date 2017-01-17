ed13d58a817540b066813d6df34e98b5b706d20b
Siddharth Mittal <smittal6@gmail.com>
2017-01-17 15:20:57 +0530

    Add bag of words model

---
+++ bagofwords.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+count=CountVectorizer(ngram_range=(1,1))
+docs=np.array(['The sun is shining','The weather is sweet','The sun is shining and the weather is sweet'])
+bag=count.fit_transform(docs)
+#So here is the deal, we take all the unique words, give them all some special mapping
+#and now to calculate feature vectors, we mark the occurences of the words
+#Thus the vectors may be sparse.
+# print(count.vocabulary_)
+# print(bag.toarray())
+#Mention of raw term frequencies. n-gram method. Taking 1 word or 2 words et al.
+#Term frequency and inverse document frequency: Downweight frequently occuring words in feature vectors
+# tf-idf=tf*idf
+#idf=log((n_d)/(1+df(d,t)))
+#We also have tf-idf library
+tfidf= TfidfTransformer()
+bag=tfidf.fit_transform(bag)
+print(bag.toarray())
